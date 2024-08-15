a55a2eb0700217ce88e93f6e2ca56e44eda5fd5f
RomanLizak <122176282+RomanLizak@users.noreply.github.com>
2024-08-15 17:02:58 +0200

    x range is global for plot, not local for each axis, plot function created, main axis is the one with the lowest index, custom limits- if not auto, then the limit from hmi is applied

--- myFigure.py
+++ myFigure.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import ast
+import numpy as np
 
 class AddFigure():
     main_axis = 1
@@ -9,8 +10,10 @@ class AddFigure():
     def __init__(self, name):
         self.name = name
         self.axes = []
+        self.x_range = [0,0]
         # holds widget refferences of each axis from frame_plot_settings in format: {'figure_name': {'parameter': widget_ref}}
         self.plot_settings = {}
+        self.figure_settings = {}
         AddFigure.figure_names.append(name)
         
 
@@ -18,8 +21,7 @@ class AddFigure():
         ax_dict = {
             'name': name,
             'axis_label': name,
-            'x_limit': None,
-            'y_limit': None,
+            'y_range_max': None,
             'legend_label': name,
             'y_ax_number': 1,
             'color': None,
@@ -29,28 +31,27 @@ class AddFigure():
         }
         self.axes.append(ax_dict)
 
-    def update_axis(self, name, axis_label = None, legend_label = None, x_limit=None, y_limit = None, y_ax_number=None, color = None, line_width=None, hide_axis=None):
+    def update_axis(self, name, axis_label = None, legend_label = None, y_range_min = None, y_range_max = None, y_ax_number=None, color = None, line_width=None, hide_axis=None):
 
         for ax in self.axes:
-            for ax in self.axes:
-                if ax['name'] == name:
-                    if axis_label is not None:
-                        ax['axis_label'] = axis_label
-                    if legend_label is not None:
-                        ax['legend_label'] = legend_label
-                    if x_limit is not None:
-                        ax['x_limit'] = x_limit
-                    if y_limit is not None:
-                        ax['y_limit'] = y_limit
-                    if y_ax_number is not None:
-                        ax['y_ax_number'] = y_ax_number
-                    if color is not None:
-                        ax['color'] = color
-                    if line_width is not None:
-                        ax['line_width'] = line_width
-                    if line_width is not None:
-                        ax['hide_axis'] = hide_axis
-                    break
+            if ax['name'] == name:
+                if axis_label is not None:
+                    ax['axis_label'] = axis_label
+                if legend_label is not None:
+                    ax['legend_label'] = legend_label
+                if y_range_min is not [None,""]:
+                    ax['y_range_min'] = y_range_min
+                if y_range_max is not [None,""]:
+                    ax['y_range_max'] = y_range_max
+                if y_ax_number is not None:
+                    ax['y_ax_number'] = y_ax_number
+                if color is not None:
+                    ax['color'] = color
+                if line_width is not None:
+                    ax['line_width'] = line_width
+                if line_width is not None:
+                    ax['hide_axis'] = hide_axis
+                break
 
     def get_info(self):
         print(self.num_of_axis)
@@ -62,35 +63,107 @@ class AddFigure():
         self.axes.sort(key=lambda x: x['y_ax_number'])
 
     def show_figure(self, preview=False):
+        print('***************start plotting****************')
+        self.axes.sort(key=lambda x: x['y_ax_number'])
+        print('sorted list of axes\n\n')
         
-        #self.axes.sort(key=lambda x: x['y_ax_number'])
-        
-        print(self.name)
-        for ax in self.axes:
-            print(ax['name'])
-        
-        return
         plt.ioff()  # Turn off interactive mode
         plt.close('all')  # Close all existing figures
-        fig, ax = plt.subplots()
-        fig.set_size_inches(12, 8)
+        
 
+        """DOPLN PARAMETER GRID KU OSIAM?"""
         legend = []
         labels =[]
+                
+        # index of the main axis
+        used_axes = list(set(int(axis['y_ax_number']) for axis in self.axes))
+        print(f'used axes are {used_axes}')       
 
-        # main axis first
-        for index, axis in self.axes:
-            if cls.main_axis == axis.y_ax_number:
-                line, = ax.plot(dict['x_value'], dict['value'], label=dict['name'], color=dict['color'], antialiased=True, linewidth=int(dict['line_width']))
-                if dict['y_limit'] != None:
-                    y_limit_list = ast.literal_eval(dict['y_limit'])
-                    ax.set_ylim(y_limit_list[0], y_limit_list[1])
+        # how many axes we need
+        number_of_axis = len(used_axes)
+        ax = [None] * (number_of_axis+1)
         
-        # all other axes
-        for index, axis in self.axes:
-            if cls.main_axis != axis.y_ax_number:
-                j=1
+        #DOKONCI HIDE AXIS a GRID!!!
 
+        fig, ax[0] = plt.subplots()
+        fig.set_size_inches(12, 8)
+        
+        print('\n\n*********LIMITS CALCULATING**********\n')
+        #calculate y limits for each axis
+        y_ranges_by_axis = {}
+        for index, axis_number in enumerate(used_axes):
+            y_range_axis = []
+            for axis in self.axes:
+                if int(axis['y_ax_number']) == axis_number:
+                    print(axis['y_range_min'], axis['y_range_max'])
+                    y_range = [axis['y_range_min'], axis['y_range_max']]
+                    y_range_axis.append(y_range)
+            y_ranges_by_axis[axis_number] = y_range_axis
+            
+        print(y_ranges_by_axis.items())
+        print('\n\n*********END OF LIMITS CALCULATING**********\n')
+        
+
+        for index, axis_number in enumerate(used_axes):
+            if index > 0:
+                ax[index] = ax[0].twinx()  # Create a new y-axis for each subsequent index
+           
+            for axis in self.axes:
+                if int(axis['y_ax_number']) == axis_number:
+                    print(f"Plotting on axis {index}: {axis['name']} (y_ax_number: {axis['y_ax_number']})")
+                    line, = ax[index].plot(
+                        axis['data'].astype(float), 
+                        label=axis['axis_label'], 
+                        color=axis['color'], 
+                        antialiased=True, 
+                        linewidth=int(axis['line_width'])
+                    )
+                    ax[index].set_ylabel(axis['axis_label'])
+                    ax[index].spines['right'].set_position(('outward', 60*index))
+                    legend.append(line)
+                    labels.append(axis['legend_label'])
+            
+            ax[0].grid(True, which='major', axis='both')
+
+        print('index, axis_index in enumerate(used_axes):')
+        for index, axis_index in enumerate(used_axes):
+                print(f'index {index} axis_index {axis_index}')
+                range_mins = []
+                range_maxes = []
+                for mins, maxes in y_ranges_by_axis[axis_index]:
+                    print(f'mins {mins}, maxes {maxes}')
+                    range_mins.append(mins)
+                    range_maxes.append(maxes)
+                print(min(range_mins), max(range_maxes))
+                
+                ax[index].set_xlim(self.x_range[0], self.x_range[1])
+                ax[index].set_ylim(min(range_mins), max(range_maxes))
+        """# Customize the grid
+            ax[0].grid(True)  # Turn the grid on
+
+            # Set the major ticks for smaller squares
+            ax[0].set_xticks(np.arange(0, 4000, 200))  # Smaller x-ticks, change 0.5 to a smaller value for finer grid
+            ax[0].set_yticks(np.arange(-1, 1.1, 0.2))  # Smaller y-ticks, change 0.2 to a smaller value for finer grid
+
+            # Set the minor ticks
+            ax[0].set_xticks(np.arange(0, 4000, 10), minor=True)  # Even finer grid on x-axis
+            ax[0].set_yticks(np.arange(-1, 1.1, 0.05), minor=True)  # Even finer grid on y-axis
+
+            # Customize the grid lines (Optional)
+            ax[0].grid(which='major', color='gray', linestyle='-', linewidth=0.5)
+            ax[0].grid(which='minor', color='gray', linestyle=':', linewidth=0.2)"""
+        ax[0].legend(legend, labels, loc='upper right', ncol=4)
+        
+        plt.title(self.name)
+        plt.tight_layout()
+        plt.ion()
+        plt.show()
+
+        
+
+
+        return
+        
         for i, dict in enumerate(self.axes):
             if dict['sep_y']:
                 ax2 = ax.twinx()
